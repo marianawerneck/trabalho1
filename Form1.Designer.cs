6cef0449f17a97442a5a0f6d3af27c6fdc425701
marianawerneck <mwerneckroque00@gmail.com>
2022-04-11 08:39:52 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace trabalho1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.shortDescription = new System.Windows.Forms.TextBox();
+            this.handle = new System.Windows.Forms.TextBox();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 148);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(776, 290);
+            this.output.TabIndex = 0;
+            this.output.Text = "";
+            // 
+            // shortDescription
+            // 
+            this.shortDescription.Location = new System.Drawing.Point(351, 33);
+            this.shortDescription.Name = "shortDescription";
+            this.shortDescription.Size = new System.Drawing.Size(125, 27);
+            this.shortDescription.TabIndex = 1;
+            // 
+            // handle
+            // 
+            this.handle.Location = new System.Drawing.Point(351, 67);
+            this.handle.Name = "handle";
+            this.handle.Size = new System.Drawing.Size(125, 27);
+            this.handle.TabIndex = 4;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(382, 100);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(94, 29);
+            this.submitBtn.TabIndex = 5;
+            this.submitBtn.Text = "submit";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "number1";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "number2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.submitBtn);
+            this.Controls.Add(this.handle);
+            this.Controls.Add(this.shortDescription);
+            this.Controls.Add(this.output);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.TextBox shortDescription;
+        private System.Windows.Forms.TextBox handle;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using ServiceReference2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabalho1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void submitBtn_Click(object sender, EventArgs e)
+        {
+            this.output.Text = "Querying the web service...";
+            int a = int.Parse(this.shortDescription.Text);
+            int b = int.Parse(this.handle.Text);
+            CalculatorSoapClient soap = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+            int result = await soap.AddAsync(a, b);
+            this.output.Text += "\nResultado soma: " + result.ToString();
+            result = await soap.DivideAsync(a, b);
+            this.output.Text += "\nResultado divisão: " + result.ToString();
+            result = await soap.MultiplyAsync(a, b);
+            this.output.Text += "\nResultado multiplicação: " + result.ToString();
+            result = await soap.SubtractAsync(a, b);
+            this.output.Text += "\nResultado subtração: " + result.ToString();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
